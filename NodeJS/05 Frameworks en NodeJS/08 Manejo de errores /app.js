a31000b5c1e8d3b9a28f712965adfd53a6a301e6
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-07-14 13:36:54 -0500

    Aplicación manejo de errores part-4

---
+++ NodeJS/05 Frameworks en NodeJS/08 Manejo de errores /app.js
@@ -0,0 +1,27 @@
+const express = require('express'),
+    favicon = require('serve-favicon'),
+    morgan = require('morgan'),
+    routes = require('./routes/index'),
+    faviconURL = `${__dirname}/public/img/node-js.svg`,
+    publicDir = express.static(`${__dirname}/public`),
+    viewDir = `${__dirname}/views`,
+    port = (process.env.PORT || 3000),
+    app = express();
+
+// console.log(`${__dirname}/public/img/node-js.svg`);
+
+app
+    // Configurando app
+    .set('views', viewDir)
+    .set('view engine', 'pug')
+    // .set('view engine', 'ejs')
+    .set('port', port)
+    // Ejecutando middlewares
+    .use(favicon(faviconURL))
+    .use(morgan('dev'))
+    .use(publicDir)
+    // Ejecuto el middleware Enrutador
+    .use('/', routes)
+
+
+module.exports = app
