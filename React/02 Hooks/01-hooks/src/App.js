cddf70bb9062c4c7bb54a90631984492ff614460
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-08-05 21:39:07 -0500

    Hooks

---
+++ React/02 Hooks/01-hooks/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hooks</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React
+        </a>
+        <hr />
+      </header>
+    </div>
+  );
+}
+
+export default App;
