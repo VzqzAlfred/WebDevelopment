8b33934cab29d5b746e5a03c1bd137edc5266b6c
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-08-10 20:45:03 -0500

    Uso de Frameworks Bootstrap & Bulma

---
+++ React/04 Estilos y frameworks/03-framework-css-react/src/App.js
@@ -0,0 +1,15 @@
+import Bootstrap from './components/Bootstrap';
+import './App.css';
+import Bulma from './components/Bulma';
+
+function App() {
+  return (
+     <div>
+          <h1>Frameworks CSS con React</h1>     
+          {/* <Bootstrap /> */}
+          <Bulma />
+     </div>
+  );
+}
+
+export default App;
