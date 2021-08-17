aeee65db43fd515c96673a20b0a763674385f28f
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-08-17 18:31:07 -0500

    API en desuso

---
+++ React/03 Ejercicios Hooks/04-Buscador-de-Canciones/03-peticiones-ajax/src/components/SongSearch.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import { SongForm } from './SongForm';
+import { SongDetail } from './SongDetail';
+import { Loader } from './Loader';
+import {helpHttp} from '../helpers/helpHttp';
+
+export const SongSearch = () => {
+     const [search, setSearch] = useState(null);
+     const [lyric, setLyric] = useState(null);
+     const [bio, setBio] = useState(null);
+     const [loading, setLoading] = useState(false);
+
+     useEffect(() => {
+          if (search === null) return;
+
+          const fetchData = async () => {
+               const {artist, song} = search;
+
+               let artistUrl = `theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
+               let songUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+               console.log(artistUrl);
+               console.log(songUrl);
+
+               setLoading(true);
+               const [artistRes, songRes] = await Promise.all([
+                    helpHttp().get(artistUrl), 
+                    helpHttp().get(songUrl)
+               ]);
+
+               console.log(artistRes);
+               console.log(songRes);
+               
+               setBio(artistRes);
+               setLyric(songRes);
+               setLoading(false);
+          }
+
+          fetchData();
+     }, [search]);
+
+     const handleSearch = (data) => {
+          console.log(data);
+          setSearch(data);
+     }
+
+     return (
+          <div>
+               <h2>Song Search</h2>
+               {loading && <Loader />}
+               <SongForm handleSearch={handleSearch} />
+               <SongDetail search={search} lyric={lyric} bio={bio} />
+          </div>
+     )
+}
