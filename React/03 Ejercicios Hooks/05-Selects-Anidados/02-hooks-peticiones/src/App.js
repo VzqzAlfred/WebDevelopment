b11b748140815cca633c85c149a64ff68214852a
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-08-18 13:59:03 -0500

    Hooks personalizados para peticiones Fetch

---
+++ React/03 Ejercicios Hooks/05-Selects-Anidados/02-hooks-peticiones/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import {SelectsAnidados} from './components/SelectsAnidados'; 
+
+function App() {
+  return (
+    <div>
+         <SelectsAnidados />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/03 Ejercicios Hooks/05-Selects-Anidados/02-hooks-peticiones/src/components/SelectList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const SelectList = () => {
+     return (
+          <div>
+               <select name="" id="">
+                    <option value="">- - -</option>
+               </select>
+          </div>
+     )
+}
