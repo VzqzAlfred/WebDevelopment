66f0088fc9b3d73c24c9aeaa2f91f81fcd2d4d8e
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-08-20 15:02:42 -0500

    Funcionalidad y reutlización de componentes

---
+++ React/03 Ejercicios Hooks/07-Ventana-Modal/03-funcionalidad-renderizado-componentes/src/Components/Modals.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useModal } from '../Hooks/useModal'
+import { Modal } from './Modal'
+
+export const Modals = () => {
+     const [isOpenModal1, openModal1, closeModal1] = useModal(false);
+     const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+     const [isOpenModal3, openModal3, closeModal3] = useModal(false);
+
+
+     return (
+          <div>
+               <h1>Modals</h1>
+               <button onClick={openModal1}>Modal 1</button>
+         <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
+              <h2>Modal 1</h2>
+              <p>Hi, this is the content of the modal 1</p>
+              <img src="https://placeimg.com/400/400/animals" alt="Animal" />
+         </Modal>
+         <button onClick={openModal2}>Modal 2</button>
+         <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
+               <h2>Modal 2</h2>
+               <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi magnam repudiandae, impedit illum non, consequuntur numquam saepe natus accusantium quidem dolores aliquid laborum perferendis? Dolores praesentium totam id doloremque minus!</p>
+               <img src="https://placeimg.com/400/400/nature" alt="Nature" />
+         </Modal>
+         <button onClick={openModal3}>Modal 3</button>
+         <Modal isOpen={isOpenModal3} closeModal={closeModal3}>
+               <h2>Modal 3</h2>
+               <p>Test for another modal</p>
+               <img src="https://placeimg.com/400/400/arch" alt="Arch" />
+         </Modal>
+          </div>
+     )
+}
