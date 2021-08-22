cd2adb00da4112b4773f797f460c66dab1ebe4ac
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-08-22 14:58:34 -0500

    Parámetros de consulta (hooks useLocation & useHistory)

---
+++ React/05 React Router/06-hook-uselocation-usehistory/src/Components/ConceptsBasics.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Acerca } from "../Pages/Acerca";
+import { Contacto } from "../Pages/Contacto";
+import { Error404 } from "../Pages/Error404";
+import { Home } from "../Pages/Home";
+import Productos from "../Pages/Productos";
+import User from "../Pages/User";
+import { MenuConcepts } from "./MenuConcepts";
+
+export const ConceptsBasics = () => {
+  return (
+    <div>
+      <h2>Basics Concepts</h2>
+      <Router>
+          <MenuConcepts />
+        <Switch>
+
+          <Route exact path="/" component={Home} />
+
+          <Route exact path="/acerca" component={Acerca}/>
+
+          <Route exact path="/contacto" component={Contacto} />
+
+          <Route exact path="/user/:username" component={User} />
+
+          <Route exact path="/productos" component={Productos} />
+          
+          {/* Por jerarquía va al final */}
+          <Route path="*" component={Error404} />
+
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+
+
+// export const ConceptsBasics = () => {
+//   return (
+//     <div>
+//       <h2>Basics Concepts</h2>
+//       <Router>
+//         <Switch>
+
+//           <Route exact path="/">
+//             <h3>Home</h3>
+//             <p>Welcome to React Router</p>
+//           </Route>
+
+//           <Route exact path="/acerca">
+//             <Acerca />
+//             <p>Invocation of component</p>
+//           </Route>
+
+//           {/* <Route exact path="/contacto" component={Contacto}/> */}
+//           <Route exact path="/contacto" children={
+//                <>
+//                     <Contacto />
+//                     <p>It's a children</p>
+//                </>
+//           } />
+
+//         </Switch>
+//       </Router>
+//     </div>
+//   );
+// };
