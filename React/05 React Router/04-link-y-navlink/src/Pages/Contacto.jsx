94d7d0b5867a45e83a0749bd3975c8677b721910
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-08-22 13:20:10 -0500

    Componentes Link & NavLink

---
+++ React/05 React Router/04-link-y-navlink/src/Pages/Contacto.jsx
@@ -0,0 +1,8 @@
+export const Contacto = () => {
+     return (
+          <div>
+               <h3>Contacto</h3>
+               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic recusandae maiores repudiandae mollitia sunt, reiciendis iure nam veritatis dolorem cum dolores quas officiis ut fugit vel expedita doloremque nesciunt in?</p>
+          </div>
+     )
+}
