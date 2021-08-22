907ced6f9e60b21b149a97246036b0ad65190913
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-08-22 14:02:00 -0500

    Paso de parámetros (hook useParams)

---
+++ React/05 React Router/05-hook-useparams/src/App.js
@@ -0,0 +1,15 @@
+import { ConceptsBasics } from "./Components/ConceptsBasics";
+
+function App() {
+  return (
+    <div>
+         <h1>React Router</h1>
+         <a href="https://reactrouter.com/web/guides/quick-start" target="_blank" rel="noreferrer">Documentation React Router</a>
+
+         <hr />
+         <ConceptsBasics />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/05 React Router/05-hook-useparams/src/Pages/Home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+     return (
+          <div>
+               <h3>Home</h3>
+               <p>Welcome to React Router</p>
+          </div>
+     )
+}
