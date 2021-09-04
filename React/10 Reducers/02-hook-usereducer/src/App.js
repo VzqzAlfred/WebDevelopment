96cc3bfd7f8933fc10ffc2b9809175c840e23e5a
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-09-04 14:39:01 -0500

    Hook useReducer

---
+++ React/10 Reducers/02-hook-usereducer/src/App.js
@@ -0,0 +1,13 @@
+import Counter from "./Components/Counter";
+
+function App() {
+  return (
+    <div>
+         <h1>useReducer</h1>
+         <hr />
+         <Counter />
+    </div>
+  );
+}
+
+export default App;
