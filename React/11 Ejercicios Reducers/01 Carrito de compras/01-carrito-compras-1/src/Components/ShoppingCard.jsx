74881418076cb1c3a53cf78fb93e504df38d9886
VzqzAlfred <alfred.vzqz96@outlook.com>
2021-09-07 13:24:41 -0500

    Carrito de compras part1

---
+++ React/11 Ejercicios Reducers/01 Carrito de compras/01-carrito-compras-1/src/Components/ShoppingCard.jsx
@@ -0,0 +1,15 @@
+const ShoppingCard = () => {
+     return (
+          <div>
+               <h1>Carrito de compras</h1>
+               <h2>Productos</h2>
+               <article className="box">
+                    
+               </article>
+               <h3>Carrito</h3>
+               <article className="box"></article>
+          </div>
+     )
+}
+
+export default ShoppingCard
---
+++ React/11 Ejercicios Reducers/01 Carrito de compras/01-carrito-compras-1/src/Reducers/shoppingReducer.jsx
@@ -0,0 +1,53 @@
+import { TYPES } from "../Actions/ShoppingActions";
+
+export const shoppingInitialState = {
+     products: [
+          {
+               id: 1,
+               name: "Producto 1",
+               price: 100
+          },
+          {
+               id: 2,
+               name: "Producto 2",
+               price: 200
+          },
+          {
+               id: 3,
+               name: "Producto 3",
+               price: 300
+          },
+          {
+               id: 4,
+               name: "Producto 4",
+               price: 400
+          },
+          {
+               id: 5,
+               name: "Producto 5",
+               price: 500
+          },
+          {
+               id: 6,
+               name: "Producto 6",
+               price: 600
+          }
+     ],
+     cart: []
+}
+
+
+export function shoppingReducer(state, action){
+     switch (action.type) {
+          case TYPES.ADD_TO_CART: {
+          }
+          case TYPES.REMOVE_ONE_FROM_CART: {
+          }
+          case TYPES.REMOVE_ALL_FROM_CART: {
+          }
+          case TYPES.CLEAR_CART: {
+          }
+          default:
+               return state;
+     }
+}
